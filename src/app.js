278a62b784caa02c32aaeebaa5f3399598e6472c
Nourhan Waleed <nouraaboelsoaoud@yahoo.com>
2023-03-29 00:41:59 +0200

    till 11

--- src/app.js
+++ src/app.js
@@ -11,19 +11,18 @@ import './styles/styles.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill' }));
-store.dispatch(addExpense({ description: 'Gas bill' }));
-store.dispatch(setTextFilter('water'));
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
 const jsx = (
-    <Provider store = {store}>
-        <AppRouter />
-    </Provider>
-    
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
 );
 
 ReactDOM.render(jsx, document.getElementById('app'));
