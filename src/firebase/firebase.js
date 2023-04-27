0cdd04428c827daf3a2c60099e26dd5c4fe97c64
Nourhan Waleed <nouraaboelsoaoud@yahoo.com>
2023-04-27 19:05:52 +0200

    mock store

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,42 +1,19 @@
 import * as firebase from 'firebase';
-
+import { initializeApp } from "firebase/app";
 const config = {
-  apiKey: "AIzaSyAapA5P8gWC83aHK26xgDDMzIBC4dw914w",
-  authDomain: "expensify-a13e6.firebaseapp.com",
-  databaseURL: "https://expensify-a13e6.firebaseio.com",
-  projectId: "expensify-a13e6",
-  storageBucket: "expensify-a13e6.appspot.com",
-  messagingSenderId: "124728096317"
+  apiKey: "AIzaSyAKVZiF9hmtp3RWsWWRU2Da8mTDDyf9t80",
+  authDomain: "expensify-60b11.firebaseapp.com",
+  databaseURL: "https://expensify-60b11-default-rtdb.firebaseio.com",
+  projectId: "expensify-60b11",
+  storageBucket: "expensify-60b11.appspot.com",
+  messagingSenderId: "941177184585",
+  appId: "1:941177184585:web:96dc8124607973f9717a98",
+  measurementId: "G-G4N74G88T0"
 };
 
-firebase.initializeApp(config);
+
+const app = initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-  name: 'Andrew Mead',
-  age: 26,
-  isSingle: false,
-  location: {
-    city: 'Philadelphia',
-    country: 'United States'
-  }
-}).then(() => {
-  console.log('Data is saved!');
-}).catch((e) => {
-  console.log('This failed.', e);
-});
-
-// database.ref().set('This is my data.');
-
-// database.ref('age').set(27);
-// database.ref('location/city').set('New York');
-
-database.ref('attributes').set({
-  height: 73,
-  weight: 150
-}).then(() => {
-  console.log('Second set call worked.');
-}).catch((e) => {
-  console.log('Things didnt for the second error', e);
-});
+export { firebase, database as default};
