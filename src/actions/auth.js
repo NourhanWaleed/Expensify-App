5b11a62126ad1740e94143a8c5f84aa0cfde9f97
Nourhan Waleed <nouraaboelsoaoud@yahoo.com>
2023-05-04 13:19:40 +0300

    routing

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,11 +1,20 @@
 import { firebase, googleAuthProvider } from '../firebase/firebase';
 
+export const login = (uid) => ({
+  type: 'LOGIN',
+  uid
+});
+
 export const startLogin = () => {
   var provider = new firebase.auth.GoogleAuthProvider();
   return () => {
     return firebase.auth().signInWithPopup(provider);
   };
 };
+
+export const logout = () => ({
+  type: 'LOGOUT'
+});
 
 export const startLogout = () => {
   return () => {
